7f49c62501f3938e88273ae037a9e20889ae4a11
viqharmohammed <31037033+viqharmohammed@users.noreply.github.com>
2017-08-21 01:41:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one',function (req,res){
       res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two',function (req,res){
+app.get('/vm',function (req,res){
        res.sendFile(path.join(__dirname, 'ui', 'vm.html'));
 });
 app.get('/article-three',function (req,res){
