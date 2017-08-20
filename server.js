238094148f8070e45dfabcf8a85ed607c4a507dd
viqharmohammed <31037033+viqharmohammed@users.noreply.github.com>
2017-08-21 00:17:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.get('/', function (req, res) {
 });
   
 app.get('/article-one',function (req,res){
-      res.send('article one reqyested and will be servered here');
+      res.send('article one requested and will be servered here');
 });
 
 app.get('/article-two',function (req,res){
-      res.send('article two reqyested and will be servered here');
+      res.send('article two requested and will be servered here');
 });
 app.get('/article-three',function (req,res){
-      res.send('article three reqyested and will be servered here');
+      res.send('article three requested and will be servered here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
