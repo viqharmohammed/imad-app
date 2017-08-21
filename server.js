62c562d03fdcc5337c8a4601e6d9f810b3e3e1d9
viqharmohammed <31037033+viqharmohammed@users.noreply.github.com>
2017-08-21 22:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
   
-app.get('/article-one',function (req,res){
+app.get('/one',function (req,res){
     res.send(createTemplate(articleone));
 });
 
